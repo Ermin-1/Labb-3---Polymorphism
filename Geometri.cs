f20e9b4345fd248c3c49444d57bd3641be620249
Ermin Husic <Ermin.Husic@live.se>
2023-10-20 09:27:57 +0200

    Add project files.

---
+++ Geometri.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb_3___Polymorphism
+{
+
+
+    internal class Geometri
+    {
+
+        public double area;
+
+        public virtual void Area()
+        {
+            area = 0;
+        }
+
+        public class Rectangle : Geometri 
+        {
+            public double Radius { get; set; }
+            public Rectangle()
+            {
+                Radius = 5;
+            }
+            public override void Area()
+            {
+                area = 0;
+            }
+        }
+
+        public class Square : Geometri
+        {
+            public double Radius { get; set; }
+
+            public Square()
+            {
+                Radius = 8;
+            }
+
+            public override void Area()
+            {
+                area = 0;
+            }
+        }
+
+        public class Circle : Geometri
+        {
+
+            public double Radius { get; set; }
+
+            public Circle()
+            {
+                Radius = 4;
+            }
+
+
+            public override void Area()
+            {
+                area = 0;
+            }
+        }
+    }
+}
